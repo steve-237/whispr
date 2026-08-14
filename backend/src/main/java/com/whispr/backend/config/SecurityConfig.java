320fea9192717a18b81758d5cac7772abadf5478
steve-237 <steve0peg@gmail.com>
2026-08-15 00:07:45 +0200

    fix: imports manquants pour @Value

--- backend/src/main/java/com/whispr/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/whispr/backend/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.whispr.backend.config;
 
 import com.whispr.backend.security.JwtAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
--- backend/src/main/java/com/whispr/backend/config/WebSocketConfig.java
+++ backend/src/main/java/com/whispr/backend/config/WebSocketConfig.java
@@ -1,6 +1,7 @@
 package com.whispr.backend.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
