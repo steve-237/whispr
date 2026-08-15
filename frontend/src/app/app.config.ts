4b3efee6ab83317b0c35a5f5cb08b1f5a8cf5f1d
steve-237 <steve0peg@gmail.com>
2026-08-15 16:18:33 +0200

    feat: setup PWA (Angular 18+)

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig, provideZonelessChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideZonelessChangeDetection, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideTranslateService } from '@ngx-translate/core';
@@ -6,13 +6,18 @@ import { provideTranslateHttpLoader } from '@ngx-translate/http-loader';
 
 import { routes } from './app.routes';
 import { authInterceptor } from './core/interceptors/auth.interceptor';
+import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideZonelessChangeDetection(), 
+    provideZonelessChangeDetection(),
     provideRouter(routes),
     provideHttpClient(withInterceptors([authInterceptor])),
     provideTranslateService(),
-    provideTranslateHttpLoader({ prefix: './assets/i18n/', suffix: '.json' })
-  ]
+    provideTranslateHttpLoader({ prefix: './assets/i18n/', suffix: '.json' }),
+    provideServiceWorker('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      registrationStrategy: 'registerWhenStable:30000',
+    }),
+  ],
 };
